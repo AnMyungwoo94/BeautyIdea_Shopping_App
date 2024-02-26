35a3d362947f93a253925cda9d946b8bf6d19387
AnMyungwoo94 <anmy121212@gmail.com>
2024-02-26 22:28:59 +0900

    Feat : 쿠폰 리사이클러뷰 연결

---
+++ app/src/main/java/com/myungwoo/shoppingmall_app/product/CouponModel.kt
@@ -0,0 +1,17 @@
+package com.myungwoo.shoppingmall_app.product
+
+data class CouponModel(
+    val name: String = "",
+    val discount: String = "",
+    val image : Int = 0,
+)
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/myungwoo/shoppingmall_app/product/CouponRvAdapter.kt
@@ -0,0 +1,47 @@
+package com.myungwoo.shoppingmall_app.product
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.myungwoo.shoppingmall_app.databinding.ItemCouponBinding
+
+class CouponRvAdapter(
+    val context: Context, private val items: MutableList<CouponModel>
+) : RecyclerView.Adapter<CouponRvAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CouponRvAdapter.ViewHolder {
+        val binding =
+            ItemCouponBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CouponRvAdapter.ViewHolder, position: Int) {
+        val binding = holder.binding
+        val itemsData = items[position]
+
+        val name = binding.nameTextView
+        val price = binding.couponTextView
+
+        name.text = itemsData.name
+        price.text = itemsData.discount
+
+        val pictureRef = Firebase.storage.reference.child("${itemsData.image}.png")
+        pictureRef.downloadUrl.addOnCompleteListener {
+            if (it.isSuccessful) {
+                Glide.with(context).load(it.result).into(binding.imageView)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(val binding: ItemCouponBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
