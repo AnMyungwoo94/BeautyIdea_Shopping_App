78e3660443cb7606749e819aae16b149c189ff8e
AnMyungwoo94 <anmy121212@gmail.com>
2024-05-24 17:54:11 +0900

    Refactor : SplashActivity Compose로 전환

--- app/src/main/java/com/myungwoo/shoppingmall_app/ui/SplashActivity.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/ui/SplashActivity.kt
@@ -1,32 +1,79 @@
 package com.myungwoo.shoppingmall_app.ui
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.myungwoo.shoppingmall_app.R
 import com.myungwoo.shoppingmall_app.ui.auth.IntroActivity
+import kotlinx.coroutines.delay
 
-class SplashActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+private lateinit var auth: FirebaseAuth
+
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        installSplashScreen()
         auth = Firebase.auth
 
-        if (auth.currentUser?.uid == null) {
-            Handler().postDelayed({
-                startActivity(Intent(this, IntroActivity::class.java))
-                finish()
-            }, 3000)
-        } else {
-            Handler().postDelayed({
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            }, 3000)
+        setContent {
+            SplashScreen()
         }
     }
 }
+
+@Composable
+fun SplashScreen() {
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        delay(1000)
+        if (auth.currentUser?.uid == null) {
+            context.startActivity(Intent(context, IntroActivity::class.java))
+        } else {
+            context.startActivity(Intent(context, MainActivity::class.java))
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.splash),
+            contentDescription = "Splash Image",
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    MaterialTheme {
+        SplashScreen()
+    }
+}
+
+
