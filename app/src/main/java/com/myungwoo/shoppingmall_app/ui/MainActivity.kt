b641a59a29d569af20de206a1f418a626dd8586e
AnMyungwoo94 <anmy121212@gmail.com>
2024-05-27 12:06:54 +0900

    Refactor : MainActivity Compose 1차 수정

--- app/src/main/java/com/myungwoo/shoppingmall_app/ui/MainActivity.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/ui/MainActivity.kt
@@ -6,22 +6,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
@@ -62,14 +62,12 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MainScreen(navController: NavHostController) {
     Scaffold(
-        containerColor = Color.White,
         bottomBar = { BottomNavigation(navController) }
     ) { padding ->
         NavHostContainer(navController, Modifier.padding(padding))
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar() {
     TopAppBar(
@@ -77,22 +75,24 @@ fun TopBar() {
             Image(
                 painter = painterResource(id = R.drawable.mainicon_peach),
                 contentDescription = "Main Icon",
-                modifier = Modifier.size(100.dp, 40.dp)
+                modifier = Modifier
+                    .size(width = 80.dp, height = 40.dp)
             )
         },
         actions = {
             AppBarAction(R.drawable.cart_icon) { /* Navigate to cart */ }
             AppBarAction(R.drawable.mypage_icon) { /* Navigate to settings */ }
         },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.White,
-        )
+        backgroundColor = Color.White
     )
 }
 
+
 @Composable
 fun AppBarAction(iconRes: Int, onClick: () -> Unit) {
-    IconButton(onClick = onClick) {
+    IconButton(
+        onClick = onClick,
+    ) {
         Icon(
             painter = painterResource(id = iconRes),
             contentDescription = null,
@@ -176,4 +176,22 @@ fun FragmentContainer(fragmentInstance: () -> Fragment) {
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    MaterialTheme {
+        TopBar()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MaterialTheme {
+        val navController = rememberNavController()
+        MainScreen(navController)
+    }
 }
