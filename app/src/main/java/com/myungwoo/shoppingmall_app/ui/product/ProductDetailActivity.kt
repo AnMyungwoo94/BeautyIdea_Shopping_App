fe105be14340798d53e0db05ca773dfe7af3ecec
AnMyungwoo94 <anmy121212@gmail.com>
2024-05-27 16:05:54 +0900

    Refactor : ProductDetailActivity Compose로 전환

--- app/src/main/java/com/myungwoo/shoppingmall_app/ui/product/ProductDetailActivity.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/ui/product/ProductDetailActivity.kt
@@ -2,108 +2,329 @@ package com.myungwoo.shoppingmall_app.ui.product
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.FirebaseStorage
 import com.myungwoo.shoppingmall_app.R
 import com.myungwoo.shoppingmall_app.data.ProductModel
-import com.myungwoo.shoppingmall_app.databinding.ActivityProductDetailBinding
-import com.myungwoo.shoppingmall_app.ui.cart.ProductCartActivity
+import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.tasks.await
 import java.text.NumberFormat
 import java.util.Locale
 
-class ProductDetailActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityProductDetailBinding
-    private var count: Int = 1
-    private var countSum: Int = 0
-    private var productPrice: Int = 0
-
+class ProductDetailActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProductDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.detailText.setMovementMethod(ScrollingMovementMethod())
-
         val receivedData = intent.getSerializableExtra("ITEM_DATA") as ProductModel
-
-        binding.productName.text = receivedData.name
-        binding.productPrice.text = "${NumberFormat.getNumberInstance(Locale.US).format(receivedData.price.toInt())} "
-        binding.productParcel.text = receivedData.parcel
-        var deliveryFee = receivedData.deliveryFee
-        if (deliveryFee != 0) {
-            binding.deliveryFee.text = "배송비 ${NumberFormat.getNumberInstance(Locale.US).format(deliveryFee)} 원"
-        } else {
-            binding.deliveryFee.text = "배송비 무료"
-        }
-        binding.productParcelDay.text = receivedData.parcelDay
-        binding.selectedProductName.text = receivedData.name
-        binding.selectedProductPrice.text = "${NumberFormat.getNumberInstance(Locale.US).format(receivedData.price.toInt())} "
-        productPrice = receivedData.price.toInt()
-
-        val pictureRef = Firebase.storage.reference.child("${receivedData.key}.png")
-        pictureRef.downloadUrl.addOnCompleteListener {
-            if (it.isSuccessful) {
-                Glide.with(this).load(it.result).into(binding.productImage)
-                Glide.with(this).load(it.result).into(binding.selectedProductImage)
+        setContent {
+            MaterialTheme {
+                ProductDetailScreen(receivedData)
             }
         }
-        binding.btnExit.setOnClickListener {
-            finish()
-        }
-        binding.plusBtn.setOnClickListener {
-            count += 1
-            updateQuantityAndTotalPrice()
-        }
+    }
+}
 
-        binding.minusBtn.setOnClickListener {
-            if (count > 1) {
-                count -= 1
-                updateQuantityAndTotalPrice()
-            }
-        }
-        binding.btnBasket.setOnClickListener {
-            val database = FirebaseDatabase.getInstance().reference
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductDetailScreen(product: ProductModel) {
+    val context = LocalContext.current
+    var count by remember { mutableStateOf(1) }
+    var countSum by remember { mutableStateOf(0) }
+    var productPrice by remember { mutableStateOf(product.price.toInt()) }
+    var imageUrl by remember { mutableStateOf<String?>(null) }
 
-            val user = FirebaseAuth.getInstance().currentUser
-            user?.let {
-                val uid = it.uid
-                val key = receivedData.key
-                val updatedProduct = receivedData.copy(count = count, count_sum = countSum)
-
-                database.child("cart").child(uid).child(key).setValue(updatedProduct)
-                    .addOnSuccessListener {
-                        Toast.makeText(this, R.string.product_detail_cart_success, Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, ProductCartActivity::class.java)
-                        startActivity(intent)
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this, R.string.product_detail_cart_fail, Toast.LENGTH_SHORT).show()
-                    }
-            }
-        }
-        binding.btnPay.setOnClickListener {
-            val intent = Intent(this, ProductPayActivity::class.java)
-            intent.putExtra("SELECTED_PRODUCT_PAY", receivedData)
-            intent.putExtra("COUNT", count)
-            intent.putExtra("COUNT_SUM", countSum)
-            intent.putExtra("origin", "DETAILS")
-            startActivity(intent)
-        }
-        updateQuantityAndTotalPrice()
+    LaunchedEffect(product.key) {
+        val storageRef = FirebaseStorage.getInstance().reference.child("${product.key}.png")
+        imageUrl = storageRef.downloadUrl.await().toString()
     }
 
-    private fun updateQuantityAndTotalPrice() {
-        findViewById<TextView>(R.id.quantity_text_view).text = count.toString()
-        val totalPrice = count * productPrice
-        binding.sumText.text = "${NumberFormat.getNumberInstance(Locale.US).format(totalPrice)} "
-        countSum = totalPrice
+    LaunchedEffect(count) {
+        countSum = count * productPrice
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.White),
+            title = { Text("상세페이지") },
+            navigationIcon = {
+                IconButton(onClick = { (context as? ComponentActivity)?.finish() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_arrow_back_ios_24),
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(32.dp)
+            ) {
+                imageUrl?.let {
+                    GlideImage(
+                        imageModel = it,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(150.dp)
+                            .height(150.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = product.name,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                    Text(
+                        text = "${
+                            NumberFormat.getNumberInstance(Locale.US).format(product.price.toInt())
+                        } 원",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 15.sp,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                    Text(
+                        text = if (product.deliveryFee != 0) "배송비 ${
+                            NumberFormat.getNumberInstance(
+                                Locale.US
+                            ).format(product.deliveryFee)
+                        } 원" else "배송비 무료",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 15.sp,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                    Text(
+                        text = product.parcel,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 15.sp,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                    Text(
+                        text = product.parcelDay,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 15.sp,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "상세 설명입니다.",
+                style = MaterialTheme.typography.bodyMedium,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.product_Detail),
+                style = MaterialTheme.typography.bodySmall,
+                fontSize = 14.sp,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+
+        Divider(
+            color = Color.Gray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "수량 합계: ${NumberFormat.getNumberInstance(Locale.US).format(countSum)} 원",
+                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier
+                ) {
+                    IconButton(
+                        onClick = { if (count > 1) count -= 1 },
+                        modifier = Modifier.size(35.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.minus_btn),
+                            contentDescription = "Minus",
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+
+                    Text(
+                        text = count.toString(),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+
+                    IconButton(
+                        onClick = { count += 1 },
+                        modifier = Modifier.size(35.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.plus_btn),
+                            contentDescription = "Plus",
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Button(
+                    border = BorderStroke(1.dp, Color.Black),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = Color.Black
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    onClick = {
+                        addToCart(product, count, countSum)
+                        Toast.makeText(context, "장바구니에 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Text(text = "장바구니")
+                }
+
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.DarkGray,
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    onClick = {
+                        val intent = Intent(context, ProductPayActivity::class.java).apply {
+                            putExtra("SELECTED_PRODUCT_PAY", product)
+                            putExtra("COUNT", count)
+                            putExtra("COUNT_SUM", countSum)
+                            putExtra("origin", "DETAILS")
+                        }
+                        context.startActivity(intent)
+                    }
+                ) {
+                    Text(text = "구매하기")
+                }
+            }
+        }
+    }
+}
+
+fun addToCart(product: ProductModel, count: Int, countSum: Int) {
+    val user = FirebaseAuth.getInstance().currentUser
+    user?.let {
+        val uid = it.uid
+        val key = product.key
+        val updatedProduct = product.copy(count = count, count_sum = countSum)
+
+        val database = FirebaseDatabase.getInstance().reference
+        database.child("cart").child(uid).child(key).setValue(updatedProduct)
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ProductDetailScreenPreview() {
+    val sampleProduct = ProductModel(
+        key = "sample_key",
+        name = "Sample Product",
+        price = "10000",
+        time = "2024-05-27",
+        parcel = "Standard",
+        deliveryFee = 0,
+        parcelDay = "3 days",
+        category = "sample_category",
+        count = 1,
+        count_sum = 1,
+        isSelected = false
+    )
+
+    MaterialTheme {
+        ProductDetailScreen(product = sampleProduct)
     }
 }
