4e755a577d8d814228ef90a03ad412657569177b
AnMyungwoo94 <anmy121212@gmail.com>
2024-06-10 18:42:50 +0900

    Remove : 필요없는 import문 제거

--- app/src/main/java/com/myungwoo/shoppingmall_app/ui/product/ProductDetailActivity.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/ui/product/ProductDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.myungwoo.shoppingmall_app.ui.product
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -22,17 +21,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.Divider
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -41,7 +37,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -57,9 +52,6 @@ import com.google.firebase.storage.FirebaseStorage
 import com.myungwoo.shoppingmall_app.R
 import com.myungwoo.shoppingmall_app.data.ProductModel
 import com.skydoves.landscapist.glide.GlideImage
-import kotlinx.coroutines.tasks.await
-import java.text.NumberFormat
-import java.util.Locale
 
 class ProductDetailActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
