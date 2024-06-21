1f334716f6a02b656936af7877a5e78c57243534
AnMyungwoo94 <anmy121212@gmail.com>
2024-06-21 12:23:10 +0900

    Refactor : collect Activity에서 실행

--- app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyActivity.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyActivity.kt
@@ -1,6 +1,7 @@
 package com.myungwoo.shoppingmall_app.piggybank
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +11,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,14 +35,21 @@ class PiggyActivity : AppCompatActivity() {
 
 @Composable
 fun PiggyScreen(viewModel: PiggyViewModel) {
-    val currentChar = viewModel.textStream.collectAsState()
+    var streamText by remember { mutableStateOf('A') }
+
+    LaunchedEffect(Unit) {
+        viewModel.zipFlow.collect { char ->
+            Log.e("PiggyActivity", "char = $char")
+            streamText = char
+        }
+    }
 
     Column(modifier = Modifier.padding(16.dp)) {
         Button(onClick = { viewModel.onButtonClick() }) {
             Text(text = "Click Me")
         }
         Text(
-            text = currentChar.value.toString(),
+            text = streamText.toString(),
             style = MaterialTheme.typography.titleMedium,
             fontSize = 50.sp,
             modifier = Modifier.padding(top = 16.dp)
--- app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
@@ -1,12 +1,9 @@
 package com.myungwoo.shoppingmall_app.piggybank
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
@@ -15,32 +12,17 @@ class PiggyViewModel : ViewModel() {
 
     private val _buttonClicked = MutableSharedFlow<Unit>()
 
-    private val _textStream = MutableStateFlow('A')
-    val textStream = _textStream.asStateFlow()
-
     private val emitTextFlow: Flow<Char> = flow {
         for (char in 'A'..'Z') {
             emit(char)
         }
     }
 
-    init {
-        startStream()
-    }
+    val zipFlow = _buttonClicked.zip(emitTextFlow) { _, char -> char }
 
     fun onButtonClick() {
         viewModelScope.launch {
             _buttonClicked.emit(Unit)
-        }
-    }
-
-    private fun startStream() {
-        viewModelScope.launch {
-            _buttonClicked.zip(emitTextFlow) { _, char -> char }
-                .collect { char ->
-                    Log.d("PiggyViewModel", "char: $char")
-                    _textStream.value = char
-                }
         }
     }
 }
