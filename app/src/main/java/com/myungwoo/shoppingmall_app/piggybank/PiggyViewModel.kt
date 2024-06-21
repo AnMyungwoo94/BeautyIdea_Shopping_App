06853bcdf02c847d85f00d5aeaa39a53d616d009
AnMyungwoo94 <anmy121212@gmail.com>
2024-06-21 17:34:41 +0900

    Feat : scan 사용해서 누적합 계산하기

--- app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.launch
 
 class PiggyViewModel : ViewModel() {
@@ -15,6 +16,7 @@ class PiggyViewModel : ViewModel() {
     val emitIntFlow: Flow<Int> = inputTextFlow
         .filter { it.toIntOrNull() != null}
         .map { it.toInt() }
+        .scan(0) { total, value -> total + value }
 
     fun onButtonClick(text: String) {
         viewModelScope.launch {
