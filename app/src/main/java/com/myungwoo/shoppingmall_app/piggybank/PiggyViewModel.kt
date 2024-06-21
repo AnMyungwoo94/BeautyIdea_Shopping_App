3e8911db64ac8e2e978b139bc3f90e90ed65d1bc
AnMyungwoo94 <anmy121212@gmail.com>
2024-06-21 14:48:12 +0900

    Feat : 소문자를 대문자로 map 연산자 사용해서 바꾸기

--- app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
+++ app/src/main/java/com/myungwoo/shoppingmall_app/piggybank/PiggyViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.launch
 
@@ -13,12 +14,14 @@ class PiggyViewModel : ViewModel() {
     private val _buttonClicked = MutableSharedFlow<Unit>()
 
     private val emitTextFlow: Flow<Char> = flow {
-        for (char in 'A'..'Z') {
+        for (char in 'a'..'z') {
             emit(char)
         }
     }
 
-    val zipFlow = _buttonClicked.zip(emitTextFlow) { _, char -> char }
+    val zipFlow: Flow<Char> = _buttonClicked
+        .zip(emitTextFlow) { _, char -> char }
+        .map { char -> char.uppercaseChar() }
 
     fun onButtonClick() {
         viewModelScope.launch {
